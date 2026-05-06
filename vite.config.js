72425a10dbe19878382d8fb56cbde81caaead8d2
Hyunoh Ryu <me@horyu.me>
2026-05-06 11:55:43 +0900

    Split production vendor chunks

--- vite.config.js
+++ vite.config.js
@@ -16,6 +16,51 @@ export default defineConfig({
   },
   build: {
     outDir: 'build',
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          minSize: 20_000,
+          maxSize: 450_000,
+          groups: [
+            {
+              name: 'react-vendor',
+              test: /node_modules[\\/](?:react|react-dom|react-router-dom|scheduler)[\\/]/,
+              priority: 40,
+            },
+            {
+              name: 'state-vendor',
+              test: /node_modules[\\/](?:react-redux|redux|@reduxjs)[\\/]/,
+              priority: 35,
+            },
+            {
+              name: 'ui-vendor',
+              test: /node_modules[\\/](?:@radix-ui|lucide-react|vaul|cmdk|class-variance-authority|clsx|tailwind-merge)[\\/]/,
+              priority: 30,
+            },
+            {
+              name: 'charts-vendor',
+              test: /node_modules[\\/](?:recharts|d3-|victory-vendor)[\\/]/,
+              priority: 30,
+            },
+            {
+              name: 'map-vendor',
+              test: /node_modules[\\/](?:leaflet|react-leaflet|@react-leaflet)[\\/]/,
+              priority: 30,
+            },
+            {
+              name: 'swagger-vendor',
+              test: /node_modules[\\/](?:swagger-ui-react|swagger-ui-dist)[\\/]/,
+              priority: 30,
+            },
+            {
+              name: 'vendor',
+              test: /node_modules[\\/]/,
+              priority: 10,
+            },
+          ],
+        },
+      },
+    },
   },
   test: {
     globals: true,
